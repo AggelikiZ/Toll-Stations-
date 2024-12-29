9b529a7919f1b549397280ada36c2c94e8891ae1
verrniko <el21440@mail.ntua.gr>
2024-12-29 18:55:39 +0200

    database linkage scripts

--- back-end/dal/db.js
+++ back-end/dal/db.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2/promise');
 
+// Use db = require('./dal/db') in each file for access to the database
+
 // create connection
 const pool = mysql.createPool({
   host: 'localhost',
