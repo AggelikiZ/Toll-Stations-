7ce31ac1e607c991228fe20f3ae1277ce6d6333a
Anastasia <askounianast@gmail.com>
2025-01-05 16:45:00 +0200

    /addPasses

--- back-end/rest_api/src/main/java/com/payway/repositories/TollStationRepository.java
+++ back-end/rest_api/src/main/java/com/payway/repositories/TollStationRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TollStationRepository extends JpaRepository<TollStation, Long> {
+public interface TollStationRepository extends JpaRepository<TollStation, String> {
 }
